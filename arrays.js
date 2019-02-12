a94250b2111e43f9942f68fe0737d4801cf7911b
Fabian Fabro <fabrofabian@gmail.com>
2019-02-12 00:49:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -45,7 +45,7 @@ function destructivelyRemoveElementFromBeginningOfArray(candyArray){
 
 function removeElementFromBeginningOfArray(candyArray){
   var newArray = candyArray;
-  newArray.pop();
+  newArray = newArray.pop();
   return newArray;
 }
 
