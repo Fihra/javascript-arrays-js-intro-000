9f2261fe6b97e0b1e73944c7e7019031ba32c9f8
Fabian Fabro <fabrofabian@gmail.com>
2019-02-12 00:16:19 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+addElementToBeginningOfArray(chocolateBars, "dove");
+destructivelyAddElementToBeginningOfArray(chocolateBars, "hersheys");
+
+function addElementToBeginningOfArray(candyArray, newCandy){
+  var newArray = candyArray;
+  newArray = [newCandy, ...newArray];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(candyArray, newCandy){
+  //var newArray;
+  return a.unshift(b);
+}
+
+function addElementToEndofArray(candyArray, newCandy){
+  return a.push(b);
+}
+
+function destructivelyAddElementToEndOfArray(candyArray, newCandy){
+  return a.push(b);
+}
+
+function accessElementInArray(){
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(){
+
+}
+
+function removeElementFromBeginningOfArray(){
+
+}
+
+function destructivelyRemoveElementFromEndOfArray(){
+
+}
+
+function removeElementFromEndOfArray(){
+
+}
